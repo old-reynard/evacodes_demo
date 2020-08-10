c9d74946fd35aa1419b02f9c50d92470e795dfff
kit <kit@hvr.world>
2020-08-10 14:38:31 -0400

    init commit

---
+++ android/app/src/main/kotlin/world/hvr/eva_codes_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package world.hvr.eva_codes_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Evacodes demo'),
+      ),
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://en.wikipedia.org/wiki/Flutter_(software)',
+        ),
+      ),
+    );
+  }
+}
